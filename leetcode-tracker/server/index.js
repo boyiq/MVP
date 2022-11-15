2563f129987f7dd7916096ddff56d616078b8d6c
Boyi Qu <boyiqu9596@gmail.com>
2022-11-15 12:41:29 -0800

    added endpoints to index.js

--- leetcode-tracker/server/index.js
+++ leetcode-tracker/server/index.js
@@ -7,7 +7,12 @@ const port = 3001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
-app.get('/test', controllers.test)
+
+app.get('/test', controllers.test);
+app.get('/problems', controllers.getAllProblems)
+app.get('/problems/:level', controllers.findProblemsByLevel);
+app.post('/problem', controllers.addNewProblem);
+app.delete('/problem', controllers.deleteProblem)
 
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`)
