afe7b87355546f796d983ac640634a39dfb3b7f3
Boyi Qu <boyiqu9596@gmail.com>
2022-11-15 18:49:45 -0800

    changed navbar to buttons

--- leetcode-tracker/src/components/Navbar.jsx
+++ leetcode-tracker/src/components/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Navbar = ()=>{
+  let navigate = useNavigate();
+
+  const handleClick = (route)=>{
+    navigate(route, {replace:true})
+  }
+
   return (
-    <div>
-      <Link to="/">Summary</Link>
-      <Link to="/problems">Problems</Link>
-      <Link to='/new'>Add Problem</Link>
+    <div className="navbar">
+      <button className="nav-link" onClick={(event)=>{
+        event.preventDefault();
+        handleClick('/')
+      }}>Summary</button>
+      <button className="nav-link" onClick={(event)=>{
+        event.preventDefault();
+        handleClick('/problems')
+      }}>Problems</button>
+      <button className="nav-link" onClick={(event)=>{
+        event.preventDefault();
+        handleClick('/new')
+      }}>Add Problems</button>
+{/*       <Link className="nav-link" to="/">Summary</Link>
+      <Link className="nav-link" to="/problems">Problems</Link>
+      <Link className="nav-link" to='/new'>Add Problem</Link> */}
     </div>
   );
 }
