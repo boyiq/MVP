1ad3adcf737e4dc0dc1f672302ae2cc38ea8897f
Boyi Qu <boyiqu9596@gmail.com>
2022-11-16 17:25:21 -0800

    wrote helper function for generating random flash cards

---
+++ leetcode-tracker/src/helpers/generateRandom.js
@@ -0,0 +1,34 @@
+import _ from 'underscore';
+
+const generateRandomCards = (array, mode)=>{
+  let unfamiliar = [];
+  let familiar = [];
+  let mastered = [];
+  let result = [];
+
+  array.forEach((element)=>{
+    if (element.familiarity === 'Unfamiliar') {
+      unfamiliar.push(element);
+    } else if (element.familiarity === 'Familiar') {
+      familiar.push(element);
+    }
+  })
+
+  if (mode === 'grind' && array.length <= 3) {
+    return array;
+  } else if (mode === 'review' && array.length <= 5) {
+    return array;
+  } else if (mode === 'grind') {
+    let newUnfamiliar = _.sample(unfamiliar, 2);
+    let newFamiliar = _.sample(familiar);
+    result = newUnfamiliar.concat(newFamiliar);
+  } else if (mode === 'review') {
+    let newUnfamiliar = _.sample(unfamiliar, 2);
+    let newFamiliar = _.sample(familiar, 2);
+    let newMastered = _.sample(mastered, 1);
+    result = newUnfamiliar.concat(newFamiliar).concat(newMastered);
+  }
+  return _.shuffle(result);
+}
+
+export default generateRandomCards;
