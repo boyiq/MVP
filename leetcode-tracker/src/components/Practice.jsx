d2d36e1f95febc4615f4eb7b825ed44bbe3464ea
Boyi Qu <boyiqu9596@gmail.com>
2022-11-17 11:58:52 -0800

    styled most of Practice page

--- leetcode-tracker/src/components/Practice.jsx
+++ leetcode-tracker/src/components/Practice.jsx
@@ -13,33 +13,53 @@ const Practice = ({allProblems, setAllProblems})=>{
   const [currCardIndex, setCurrCardIndex]=useState('');
   const [lastDur, setLastDur] = useState('');
 
+  const renderFamiliarity = (cat) => {
+    if (cat === 'Unfamiliar') {
+      return (
+        <div id="info-cat" style={{backgroundColor: "#FF7D7D", color:"#372948"}}>{flashcards[currCardIndex].familiarity}</div>
+      )
+    } else if (cat === 'Familiar') {
+      return (
+        <div id="info-cat" style={{backgroundColor: "#FAD6A5", color:"#372948"}}>{flashcards[currCardIndex].familiarity}</div>
+      )
+    } else if (cat === 'Mastered') {
+      return (
+        <div id="info-cat" style={{backgroundColor: "#B6E388", color:"#372948"}}>{flashcards[currCardIndex].familiarity}</div>
+      )
+    }
+  }
+
   if (firstLoad) {
     return (
       <div>
-        <form>
-          <label>Pick a category</label>
-          <select className="dropdown-list" onChange={(event)=>{
-            setCategory(event.target.value);
-            if (event.target.value === 'all') {
-              setSelectedProblems(allProblems);
-              setFlashcards(generateRandom(allProblems, mode));
-            } else {
-              setSelectedProblems(filterProblems(event.target.value, allProblems))
-              setFlashcards(generateRandom(filterProblems(event.target.value, allProblems), mode));
-            }
-          }}
-        >
-            <option value='all'>All</option>
-            {renderCats()}
-          </select>
-          <label>Pick a mode</label>
-          <select className="dropdown-list" onChange={(event)=>{
-            setMode(event.target.value);
-            setFlashcards(generateRandom(selectedProblems, event.target.value));
-          }}>
-            <option value="review">Review</option>
-            <option value="grind">Grind</option>
-          </select>
+        <form className="practice-selection">
+          <div className="selection">
+            <label className="label">Pick a category</label>
+            <select className="dropdown-list" onChange={(event)=>{
+              setCategory(event.target.value);
+              if (event.target.value === 'all') {
+                setSelectedProblems(allProblems);
+                setFlashcards(generateRandom(allProblems, mode));
+              } else {
+                setSelectedProblems(filterProblems(event.target.value, allProblems))
+                setFlashcards(generateRandom(filterProblems(event.target.value, allProblems), mode));
+              }
+            }}
+            >
+              <option value='all'>All</option>
+              {renderCats()}
+            </select>
+          </div>
+          <div className="selection">
+            <label className="label">Pick a mode</label>
+            <select className="dropdown-list" onChange={(event)=>{
+              setMode(event.target.value);
+              setFlashcards(generateRandom(selectedProblems, event.target.value));
+            }}>
+              <option value="review">Review</option>
+              <option value="grind">Grind</option>
+            </select>
+          </div>
           <button onClick={()=>{
             setFirstLoad(false);
             setCurrCardIndex(0);
@@ -50,12 +70,13 @@ const Practice = ({allProblems, setAllProblems})=>{
   }
 
   return (
-    <div>
-      <div className="flashcard">
-        <div>{flashcards[currCardIndex].name}</div>
-        <div>{flashcards[currCardIndex].category}</div>
-        <div>{flashcards[currCardIndex].familiarity}</div>
-        <div>Aim to solve in {flashcards[currCardIndex].target_duration} min</div>
+    <div className="flashcard">
+      <div className="card-info">
+        <div id="info-name">{flashcards[currCardIndex].name}</div>
+        <div id="info-cat">{flashcards[currCardIndex].category}</div>
+{/*         <div id="info-familiarity">{flashcards[currCardIndex].familiarity}</div> */}
+        {renderFamiliarity(flashcards[currCardIndex].familiarity)}
+        <div id="info-target">Aim to solve in {flashcards[currCardIndex].target_duration} min</div>
         <div>Last time you spent {flashcards[currCardIndex].last_duration} min solving the problem</div>
         <a href={flashcards[currCardIndex].link} target="_blank" rel="noopener noreferrer">Go to problem</a>
       </div>
@@ -70,15 +91,15 @@ const Practice = ({allProblems, setAllProblems})=>{
         </form>
       </div>
       <div>
-        <button onClick={()=>{
+        <button className="mark-status unfamiliar" onClick={()=>{
           markStatus(flashcards[currCardIndex], 'Unfamiliar', setAllProblems);
-        }}>Unfamiliar</button>
-        <button onClick={()=>{
+        }}>Mark as Unfamiliar  <i class="fa-regular fa-circle-question"></i></button>
+        <button className="mark-status familiar" onClick={()=>{
           markStatus(flashcards[currCardIndex], 'Familiar', setAllProblems);
-        }}>Familiar</button>
-        <button onClick={()=>{
+        }}>Mark as Familiar  <i class="fa-regular fa-face-laugh-squint"></i></button>
+        <button className="mark-status mastered" onClick={()=>{
           markStatus(flashcards[currCardIndex], 'Mastered', setAllProblems);
-        }}>Mastered</button>
+        }}>Mark as Mastered  <i class="fa-solid fa-bolt"></i></button>
       </div>
       <button onClick={()=>{
         if (currCardIndex !== 0) {
